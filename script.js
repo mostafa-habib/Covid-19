dc5ba00aff46f10b2072e175f328ea38d6d7eb3b
Mostafa Habib <sasahabeb86@gmail.com>
2022-01-26 18:39:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,8 @@ menu.addEventListener('click',function (){
     navbar.classList.toggle('nav-toggel');
 
 })
-let scroll1=document.querySelector('.scroll');
 window.onscroll=function (){
     menu.classList.remove('fa-times');
     navbar.classList.remove('nav-toggel');
-    if(window.scrollY > 0){
-        scroll1.classList.add('scroll');
-    }
-    else{
-        scroll1.classList.remove('scroll');
-    }
+
 }
